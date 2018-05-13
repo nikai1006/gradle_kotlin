3941c703f8a1a03adee8515a4cdcac356071e787
nikai <keni1006@163.com>
2018-05-13 15:24:22 +0800

    init project

---
+++ src/main/kotlin/kotlin_test1.kt
@@ -0,0 +1,20 @@
+/**
+ * demo1
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 17:11 2018/5/12
+ * @Modified By:
+ */
+var name = "nikai"
+val age = 20
+const val sex = "man"
+
+fun main(args: Array<String>) {
+//    age = 10 //不可变
+    name = "kaini"
+    print("hi,kaini")
+    for (i in 0..99) {
+        println()
+        print(i)
+    }
+}
