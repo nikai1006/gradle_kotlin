c63a92ef59b28eb33aaf1a7260fb99050bff1486
nikai <keni1006@163.com>
2018-05-23 23:42:24 +0800

    modified build script

---
+++ src/main/kotlin/cn/net/nikai/kotlint/demo1/controller/AreaFor.kt
@@ -0,0 +1,26 @@
+package cn.net.nikai.kotlint.demo1.controller
+
+/**
+ * demo1 cn.net.nikai.kotlint.demo1.controller
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 23:32 2018/5/23
+ * @Modified By:
+ */
+fun main(args: Array<String>) {
+    for (x in 0..5) {//声明闭区间包含0和5
+        print("$x,")
+    }
+    println()
+    for (x in 0 until 5) {//声明半开区间包含0，不包含5
+        print("$x,")
+    }
+    println()
+    for (x in 'a'..'e') {//声明闭区间包含‘a’和‘e’
+        print("$x,")
+    }
+    println()
+    for (x in 'a' until 'e') {//声明半开区间包含a，不包含e
+        print("$x,")
+    }
+}
