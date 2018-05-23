f93a343d8d70e14f8e07c0dd7783025b42554c8e
nikai <keni1006@163.com>
2018-05-24 00:27:12 +0800

    添加新类

---
+++ src/main/kotlin/cn/net/nikai/kotlint/demo1/function/VarargParamFunctionDemo.kt
@@ -0,0 +1,25 @@
+package cn.net.nikai.kotlint.demo1.function
+
+/**
+ * 可变参数函数
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 0:20 2018/5/24
+ * @Modified By:
+ */
+fun main(args: Array<String>) {
+    println(sum(100.0, 20.0, 30.0))
+    println(sum(30.0, 50.0))
+    println(sum(30.0, 40.0, multiple = 5))
+
+    val doubleAry = doubleArrayOf(40.0, 30.0, 20.0)
+    println(sum(30.0, 50.0, *doubleAry))//*展开运算符， *doubleAry吧数组展开
+}
+
+fun sum(vararg numbers: Double, multiple: Int = 1): Double {
+    var total = 0.0
+    for (number in numbers) {
+        total += number
+    }
+    return total * multiple
+}
