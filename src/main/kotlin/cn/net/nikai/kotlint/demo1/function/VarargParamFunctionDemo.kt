b0b09f9e6efe570f004812e814d5bfaf717d89ab
nikai <keni1006@163.com>
2018-05-24 00:28:37 +0800

    添加新类

--- src/main/kotlin/cn/net/nikai/kotlint/demo1/function/VarargParamFunctionDemo.kt
+++ src/main/kotlin/cn/net/nikai/kotlint/demo1/function/VarargParamFunctionDemo.kt
@@ -10,7 +10,7 @@ package cn.net.nikai.kotlint.demo1.function
 fun main(args: Array<String>) {
     println(sum(100.0, 20.0, 30.0))
     println(sum(30.0, 50.0))
-    println(sum(30.0, 40.0, multiple = 5))
+    println(sum(30.0, 40.0, multiple = 5))//可变参数不是最后一个参数时候，后面的参数需要采用命名参数形式传递
 
     val doubleAry = doubleArrayOf(40.0, 30.0, 20.0)
     println(sum(30.0, 50.0, *doubleAry))//*展开运算符， *doubleAry吧数组展开
