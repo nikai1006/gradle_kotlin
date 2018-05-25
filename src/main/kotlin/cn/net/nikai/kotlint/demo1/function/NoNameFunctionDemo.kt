5a94eb33a8c0cfe1bab10b937f3b791f5031efdb
keni <keni@aijiatui.com>
2018-05-25 14:42:08 +0800

    匿名函数 局部函数

--- src/main/kotlin/cn/net/nikai/kotlint/demo1/function/NoNameFunctionDemo.kt
+++ src/main/kotlin/cn/net/nikai/kotlint/demo1/function/NoNameFunctionDemo.kt
@@ -14,7 +14,7 @@ fun calcFun2(num1: Int, num2: Int, operator: Char): Int {
         }
     }
 
-    val resultFun2 = if (operator == '+') {
+    val resultFun2 = if (operator == '+') {//这种情况下的两个匿名函数的确切参数要一致
         fun(a1: Int, a2: Int): Int {
             return a1 + a2
         }
